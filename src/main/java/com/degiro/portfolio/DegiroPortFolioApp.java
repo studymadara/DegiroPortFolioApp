6968425062dde750fcc45876426f70816f908037
studymadara <virajwagh6@gmail.com>
2022-03-05 11:06:55 +0400

    testing deployment

--- src/main/java/com/degiro/portfolio/DegiroPortFolioApp.java
+++ src/main/java/com/degiro/portfolio/DegiroPortFolioApp.java
@@ -3,11 +3,14 @@ package com.degiro.portfolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class DegiroPortFolioApp
 {
     public static void main(String[] args)
     {
+        System.out.println(Arrays.toString(args));
         SpringApplication.run(DegiroPortFolioApp.class,args);
     }
 }
